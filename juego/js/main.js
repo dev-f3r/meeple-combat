7fdb768c3aac04f2aa43a5fc4a8e6cacd170180c
dev-f3r <devfercastro@gmail.com>
2023-10-10 22:39:19 -0300

    habilidadesDict actualizado, continuar con sus funciones

--- juego/js/main.js
+++ juego/js/main.js
@@ -202,56 +202,251 @@ document.body.addEventListener('dragstart', (e) => {
 // ? Objeto para almacenar información de las habilidades
 // TODO: Agregar habilidades restantes, y revisar existentes
 const habilidadesDict = {
-  "habilidad 1": "Habildad 1 sin descripción",
-  "habilidad 2": "Habildad 2 sin descripción",
-  "habilidad 3": "Habildad 3 sin descripción",
-  "machacar": "Impacta en el objetivo generando daño crítico y quitándole mitigación durante 1 turno <br> Poder min(6)",
-  "ira ciega": "Al matar un enemigo, puedes atacar a cualquier criatura o personaje que esté a tu alcance, sin necesidad de acciones <br> Poder min(1)",
-  "masestro de armas": "Eres competente con todas las armas, al matar un enemigo puedes hacerte con la suya <br> Habilidad Pasiva",
-  "duelo": "En combate 1 vs 1 obtienes +3 a la esquiva y ataque <br> Habilidad Pasiva",
-  "corte profundo": "Daña al objetivo y deja un sangrado que genera 3 puntos de daño por turno durante 3 turnos (no mitigables) <br> Poder min(6)",
-  "ataque rapido": "Avanza hacia el objetivo y lo ataca en una acción combinada / distancia máx(velocidad x 2) <br> Poder min(3) / Acciones(Arma)",
-  "cobertura": "Aumenta 300% la mitigación ante proyectiles físicos durante 3 turnos (al moverse se pierde la cobertura) <br> Poder(3) / Requiere Escudo",
-  "embestida con escudo": "Golpeas al objetivo generándole 200% daño físico y derribándolo<br> Poder(3) / Requiere Escudo",
-  "ataque poderoso": "Carga tu golpe con energía<br> Poder min(1) / Sin Requerimentos",
-  "ataque multiple": "Una serie de golpes, los cuales suman todo su daño, generándolo de manera explosiva al finalizar la habilidad <br> Poder min(3)",
-  "golpe de chi": "Un poderoso empujón cargado de energía, que genera daño físico, aturde al objetivo 1 turno y lo hace retroceder (ataque x casilleros)<br> Poder min(6)",
-  "patada voladora": "Te lanzas hacia el objetivo a una distancia máxima de (velocidad x casilleros), al golpearlo generas 200% de daño físico + 1 punto de daño por metro recorrido.<br> Poder min(3) / Sin Requisitos",
-  "desarmar": "Desarmas al objetivo, dejando caer su arma a 1 casillero de distancia a elección.<br> Poder(3)",
-  "sigilo": "Si tu objetivo se encuentra en combate con otro personaje o criatura, obtienes + 3 al ataque.<br> Pasiva",
-  "torbellino": "Giras tu con tu arma generando 50% de daño a todos los objetivos en tu rango de alcance, puedes caminar mientras la habilidad está activa.<br> Poder min(6)",
-  "flechas multiples": "Lanzas 3 flechas juntas, las cuales harán 50% de daño de manera acumulativa si más de un proyectil impacta en el mismo objetivo.( se debe realizar una tirada por cada flecha)<br> Poder(3) / Requiere arco",
-  "flecha energizada": "Cargas con energía tus flechas aumentando su daño.<br> Poder min(1) / Requiere arco",
-  "flecha elemental": "Cambia el tipo de daño elemental que produce tu flecha.<br> Poder (3) / Requiere arco",
-  "invocar": "Ahora puedes manifestar criaturas elementales.",
-  "golpe vampirico": "Absorbes vida del objetivo para ti mismo. <br> Poder min(6).",
-  "incansable": "Habilidad pasiva que brinda +1 acción.",
-  "ataque doble": "Realiza dos ataques, con armas gemelas en la misma cantidad de acciones. <br> Poder min(3)",
-  // Habilidades mágicas
-  "exorcismo": "Genera daño sagrado y aturde al objetivo durante 1 turno <br> Poder min(3)",
-  "bola de fuego": "Genera daño de fuego. Los golpes críticos incendian al objetivo, lo que le genera 3 puntos de daño no mitigable, durante 3 turnos. <br> Poder min(3)",
-  "bola de hielo": "Genera daño de hielo. Los golpes críticos congelan al objetivo, quitándole mitad de velocidad y toda mitigación hacia este elemento, durante 3 turnos. <br> Poder min(3)",
-  "sentencia": "El objetivo pierde toda mitigación durante 3 turno <br> Poder(3)",
-  "sanar": "Restaura al objetivo 1 x 1 puntos de salud <br> Poder min(1)",
-  "misil arcano": "Genera daño etéreo<br> Poder min(1)",
-  "explosion de escarcha": "Una onda expansiva que congela a todos los enemigos en un radio de (ataque x casillero) / lo que les reduce a la mitad su velocidad y los deja sin mitigación hacia este elemento, durante 3 turnos.<br> Poder min(6)",
-  "invisibilidad": "Te vuelves indetectable para los demás durante 3 turnos, al atacar pierdes el efecto.<br> Poder min(6)",
-  "enraizar": "Unas poderosas raíces surgen del suelo sujetando al objetivo e impidiéndole moverse del lugar durante 3 turnos.<br> Poder min(3) / El objetivo aún puede atacar, e incluso atacar a las raíces, las cuales tienen 50 puntos de vida.",
-  "envenenar": "Envenenas al objetivo generándole 100% de daño mágico durante 3 turnos.<br> Poder (6)",
-  "licantropia": "Puedes tener garras y colmillos como armas naturales durante 3 turnos.<br> Poder o Poder(3) <br> Escribe el comando: /licántropo",
-  "terremoto": "Vuelves inestable un área de (ataque x casilleros) durante 3 turnos que se renuevan con cada lanzamiento. Atravesar este terreno cuesta doble de movimiento, podrás lanzar el ataque nuevamente cada vez que alguien pase sobre el área, en caso de acertar este se caerá. Los golpes críticos de terremoto en áreas inestables inmovilizan al objetivo. <br> Poder(3)",
-  "relampago": "Genera daño de electricidad y rebota en hasta 3 objetivos que no estén separados por más de (ataque x casilleros), los golpes críticos dejan al objetivo electrificado.<br> Poder min(3)",
-  "sobrecarga": "Sobrecargas un objetivo que se encuentre electrificado generando 500% de daño mágico.<br> Poder min(3)",
-  "control mental": "Controlas el personaje o npc durante 1 turno. <br> Poder min(9)",
-  "confundir": "Confunde al objetivo, haciendo que cualquier ataque que este haga y falle, durante 3 turno, sea un golpe certero hacia el mismo o un aliado cercano. Una pifia se considera crítico para la víctima.<br> Poder min(6)",
-  "miedo": "El objetivo gasta todas sus acciones corriendo en dirección aleatoria, siempre alejándose de quien lanzó el hechizo. En caso de un escenario cerrado, correrá y quedará en la esquina más lejana.<br> Poder min(6)",
-  "tsunami": "Una poderosa ola en forma de cono que se expande hasta 1 casillero x ataque. Empuja todo a su paso a una distancia igual a la mitad del ataque y genera daño mágico de agua.<br> Poder min(9)",
-  "drenar": "Transfiere vida, energía o maná del objetivo al conjurador, puedes apostar una de estas poders para aumentar el efecto, la cual solo se consumirá si fallas el ataque.<br> Poder min(0)",
-  "transmutar": "Convierte vida energía o maná en cualquiera de ellas, puede usarse sobre si mismo o sobre otros personajes.<br> Poder min(1)",
-  "derribo": "Derriba al objetivo.<br> Poder(3)",
-  "desgarro": "Una herida que genera 3 puntos de daño no mitigable durante 3 turnos.<br> Poder(6)",
-  "triturar": "Rompe los huesos de la víctima, causando daño físico y dejando a la víctima con -1 a la velocidad durante 3 turnos, este penalizador se puede acumular, el tiempo de efecto se reinicia al hacerlo.<br> Poder(6)",
-  "petrificar": "Rompe los huesos de la víctima, causando daño físico y dejando a la víctima con -1 a la velocidad durante 3 turnos, este penalizador se puede acumular, el tiempo de efecto se reinicia al hacerlo, una vez sin velocidad el objetivo recibe doble de daño.<br> Poder(6)"
+  "habilidad 1": {
+    nombre: "habilidad 1",
+    coste: 1,
+    descripcion: "Habildad 1 sin descripción"
+  },
+  "habilidad 2": {
+    nombre: "habilidad 2",
+    coste: 1,
+    descripcion: "Habildad 2 sin descripción"
+  },
+  "habilidad 3": {
+    nombre: "habilidad 3",
+    coste: 1,
+    descripcion: "Habildad 3 sin descripción"
+  },
+  "machacar": {
+    nombre: "machacar",
+    coste: 1,
+    descripcion: "Impacta en el objetivo generando daño crítico y quitándole mitigación durante 1 turno <br> Poder min(6)"
+  },
+  "ira ciega": {
+    nombre: "ira ciega",
+    coste: 1,
+    descripcion: "Al matar un enemigo, puedes atacar a cualquier criatura o personaje que esté a tu alcance, sin necesidad de acciones <br> Poder min(1)"
+  },
+  "masestro de armas": {
+    nombre: "masestro de armas",
+    coste: 1,
+    descripcion: "Eres competente con todas las armas, al matar un enemigo puedes hacerte con la suya <br> Habilidad Pasiva"
+  },
+  "duelo": {
+    nombre: "duelo",
+    coste: 1,
+    descripcion: "En combate 1 vs 1 obtienes +3 a la esquiva y ataque <br> Habilidad Pasiva"
+  },
+  "corte profundo": {
+    nombre: "corte profundo",
+    coste: 1,
+    descripcion: "Daña al objetivo y deja un sangrado que genera 3 puntos de daño por turno durante 3 turnos (no mitigables) <br> Poder min(6)"
+  },
+  "ataque rapido": {
+    nombre: "ataque rapido",
+    coste: 1,
+    descripcion: "Avanza hacia el objetivo y lo ataca en una acción combinada / distancia máx(velocidad x 2) <br> Poder min(3) / Acciones(Arma)"
+  },
+  "cobertura": {
+    nombre: "cobertura",
+    coste: 1,
+    descripcion: "Aumenta 300% la mitigación ante proyectiles físicos durante 3 turnos (al moverse se pierde la cobertura) <br> Poder(3) / Requiere Escudo"
+  },
+  "embestida con escudo": {
+    nombre: "embestida con escudo",
+    coste: 1,
+    descripcion: "Golpeas al objetivo generándole 200% daño físico y derribándolo <br> Poder(3) / Requiere Escudo"
+  },
+  "ataque poderoso": {
+    nombre: "ataque poderoso",
+    coste: 1,
+    descripcion: "Carga tu golpe con energía <br> Poder min(1) / Sin Requerimentos"
+  },
+  "ataque multiple": {
+    nombre: "ataque multiple",
+    coste: 1,
+    descripcion: "Una serie de golpes, los cuales suman todo su daño, generándolo de manera explosiva al finalizar la habilidad <br> Poder min(3)"
+  },
+  "golpe de chi": {
+    nombre: "golpe de chi",
+    coste: 1,
+    descripcion: "Un poderoso empujón cargado de energía, que genera daño físico, aturde al objetivo 1 turno y lo hace retroceder (ataque x casilleros) <br> Poder min(6)"
+  },
+  "patada voladora": {
+    nombre: "patada voladora",
+    coste: 1,
+    descripcion: "Te lanzas hacia el objetivo a una distancia máxima de (velocidad x casilleros), al golpearlo generas 200% de daño físico + 1 punto de daño por metro recorrido <br> Poder min(3) / Sin Requisitos"
+  },
+  "desarmar": {
+    nombre: "desarmar",
+    coste: 1,
+    descripcion: "Desarmas al objetivo, dejando caer su arma a 1 casillero de distancia a elección <br> Poder(3)"
+  },
+  "sigilo": {
+    nombre: "sigilo",
+    coste: 1,
+    descripcion: "Si tu objetivo se encuentra en combate con otro personaje o criatura, obtienes + 3 al ataque <br> Pasiva"
+  },
+  "torbellino": {
+    nombre: "torbellino",
+    coste: 1,
+    descripcion: "Giras tu con tu arma generando 50% de daño a todos los objetivos en tu rango de alcance, puedes caminar mientras la habilidad está activa <br> Poder min(6)"
+  },
+  "flechas multiples": {
+    nombre: "flechas multiples",
+    coste: 1,
+    descripcion: "Lanzas 3 flechas juntas, las cuales harán 50% de daño de manera acumulativa si más de un proyectil impacta en el mismo objetivo (se debe realizar una tirada por cada flecha) <br> Poder(3) / Requiere arco"
+  },
+  "flecha energizada": {
+    nombre: "flecha energizada",
+    coste: 1,
+    descripcion: "Cargas con energía tus flechas aumentando su daño <br> Poder min(1) / Requiere arco"
+  },
+  "flecha elemental": {
+    nombre: "flecha elemental",
+    coste: 1,
+    descripcion: "Cambia el tipo de daño elemental que produce tu flecha <br> Poder(3) / Requiere arco"
+  },
+  "invocar": {
+    nombre: "invocar",
+    coste: 1,
+    descripcion: "Ahora puedes manifestar criaturas elementales"
+  },
+  "golpe vampirico": {
+    nombre: "golpe vampirico",
+    coste: 1,
+    descripcion: "Absorbes vida del objetivo para ti mismo <br> Poder min(6)"
+  },
+  "incansable": {
+    nombre: "incansable",
+    coste: 1,
+    descripcion: "Habilidad pasiva que brinda +1 acción"
+  },
+  "ataque doble": {
+    nombre: "ataque doble",
+    coste: 1,
+    descripcion: "Realiza dos ataques, con armas gemelas en la misma cantidad de acciones <br> Poder min(3)"
+  },
+  "exorcismo": {
+    nombre: "exorcismo",
+    coste: 1,
+    descripcion: "Genera daño sagrado y aturde al objetivo durante 1 turno <br> Poder min(3)"
+  },
+  "bola de fuego": {
+    nombre: "bola de fuego",
+    coste: 1,
+    descripcion: "Genera daño de fuego. Los golpes críticos incendian al objetivo, lo que le genera 3 puntos de daño no mitigable, durante 3 turnos <br> Poder min(3)"
+  },
+  "bola de hielo": {
+    nombre: "bola de hielo",
+    coste: 1,
+    descripcion: "Genera daño de hielo. Los golpes críticos congelan al objetivo, quitándole mitad de velocidad y toda mitigación hacia este elemento, durante 3 turnos <br> Poder min(3)"
+  },
+  "sentencia": {
+    nombre: "sentencia",
+    coste: 1,
+    descripcion: "El objetivo pierde toda mitigación durante 3 turno <br> Poder(3)"
+  },
+  "sanar": {
+    nombre: "sanar",
+    coste: 1,
+    descripcion: "Restaura al objetivo 1 x 1 puntos de salud <br> Poder min(1)"
+  },
+  "misil arcano": {
+    nombre: "misil arcano",
+    coste: 1,
+    descripcion: "Genera daño etéreo <br> Poder min(1)"
+  },
+  "explosion de escarcha": {
+    nombre: "explosion de escarcha",
+    coste: 1,
+    descripcion: "Una onda expansiva que congela a todos los enemigos en un radio de (ataque x casillero) / lo que les reduce a la mitad su velocidad y los deja sin mitigación hacia este elemento, durante 3 turnos <br> Poder min(6)"
+  },
+  "invisibilidad": {
+    nombre: "invisibilidad",
+    coste: 1,
+    descripcion: "Te vuelves indetectable para los demás durante 3 turnos, al atacar pierdes el efecto <br> Poder min(6)"
+  },
+  "enraizar": {
+    nombre: "enraizar",
+    coste: 1,
+    descripcion: "Unas poderosas raíces surgen del suelo sujetando al objetivo e impidiéndole moverse del lugar durante 3 turnos <br> Poder min(3) / El objetivo aún puede atacar, e incluso atacar a las raíces, las cuales tienen 50 puntos de vida"
+  },
+  "envenenar": {
+    nombre: "envenenar",
+    coste: 1,
+    descripcion: "Envenenas al objetivo generándole 100% de daño mágico durante 3 turnos <br> Poder (6)"
+  },
+  "licantropia": {
+    nombre: "licantropia",
+    coste: 1,
+    descripcion: "Puedes tener garras y colmillos como armas naturales durante 3 turnos <br> Poder o Poder(3) <br> Escribe el comando: /licántropo"
+  },
+  "terremoto": {
+    nombre: "terremoto",
+    coste: 1,
+    descripcion: "Vuelves inestable un área de (ataque x casilleros) durante 3 turnos que se renuevan con cada lanzamiento. Atravesar este terreno cuesta doble de movimiento, podrás lanzar el ataque nuevamente cada vez que alguien pase sobre el área, en caso de acertar este se caerá. Los golpes críticos de terremoto en áreas inestables inmovilizan al objetivo <br> Poder(3)"
+  },
+  "relampago": {
+    nombre: "relampago",
+    coste: 1,
+    descripcion: "Genera daño de electricidad y rebota en hasta 3 objetivos que no estén separados por más de (ataque x casilleros), los golpes críticos dejan al objetivo electrificado <br> Poder min(3)"
+  },
+  "sobrecarga": {
+    nombre: "sobrecarga",
+    coste: 1,
+    descripcion: "Sobrecargas un objetivo que se encuentre electrificado generando 500% de daño mágico <br> Poder min(3)"
+  },
+  "control mental": {
+    nombre: "control mental",
+    coste: 1,
+    descripcion: "Controlas el personaje o npc durante 1 turno <br> Poder min(9)"
+  },
+  "confundir": {
+    nombre: "confundir",
+    coste: 1,
+    descripcion: "Confunde al objetivo, haciendo que cualquier ataque que este haga y falle, durante 3 turno, sea un golpe certero hacia el mismo o un aliado cercano. Una pifia se considera crítico para la víctima <br> Poder min(6)"
+  },
+  "miedo": {
+    nombre: "miedo",
+    coste: 1,
+    descripcion: "El objetivo gasta todas sus acciones corriendo en dirección aleatoria, siempre alejándose de quien lanzó el hechizo. En caso de un escenario cerrado, correrá y quedará en la esquina más lejana <br> Poder min(6)"
+  },
+  "tsunami": {
+    nombre: "tsunami",
+    coste: 1,
+    descripcion: "Una poderosa ola en forma de cono que se expande hasta 1 casillero x ataque. Empuja todo a su paso a una distancia igual a la mitad del ataque y genera daño mágico de agua <br> Poder min(9)"
+  },
+  "drenar": {
+    nombre: "drenar",
+    coste: 1,
+    descripcion: "Transfiere vida, energía o maná del objetivo al conjurador, puedes apostar una de estas poders para aumentar el efecto, la cual solo se consumirá si fallas el ataque <br> Poder min(0)"
+  },
+  "transmutar": {
+    nombre: "transmutar",
+    coste: 1,
+    descripcion: "Convierte vida energía o maná en cualquiera de ellas, puede usarse sobre si mismo o sobre otros personajes <br> Poder min(1)"
+  },
+  "derribo": {
+    nombre: "derribo",
+    coste: 1,
+    descripcion: "Derriba al objetivo <br> Poder(3)"
+  },
+  "desgarro": {
+    nombre: "desgarro",
+    coste: 1,
+    descripcion: "Una herida que genera 3 puntos de daño no mitigable durante 3 turnos <br> Poder(6)"
+  },
+  "triturar": {
+    nombre: "triturar",
+    coste: 1,
+    descripcion: "Rompe los huesos de la víctima, causando daño físico y dejando a la víctima con -1 a la velocidad durante 3 turnos, este penalizador se puede acumular, el tiempo de efecto se reinicia al hacerlo <br> Poder(6)"
+  },
+  "petrificar": {
+    nombre: "petrificar",
+    coste: 1,
+    descripcion: "Rompe los huesos de la víctima, causando daño físico y dejando a la víctima con -1 a la velocidad durante 3 turnos, este penalizador se puede acumular, el tiempo de efecto se reinicia al hacerlo, una vez sin velocidad el objetivo recibe doble de daño <br> Poder(6)"
+  }
 }
 
 // ? Objeto para almacenar información de las armas
