16f47df80f0784be2d77af7863ff42bf65fc99d3
Fernando Castro <devfercastro@gmail.com>
2024-02-27 14:20:41 -0300

    Nose

---
+++ interfaz/js/UI.js
@@ -0,0 +1,11 @@
+const qsyo = `<div class="contenedor-input" id="contenedorInputComandos" style="display: none;">
+    <span id="inputLabelComandos" class="input-label">Ingrese comando</span>
+    <input type="text" id="comandosValor" class="comandos-input">
+    <button id="ingresarComandos" class="input-button">Ingresar</button>
+</div>`
+
+let form = ""
+
+function generar(el, id, tlo, txtId, btnId) {
+    
+}
